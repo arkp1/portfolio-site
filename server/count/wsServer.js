5d5650b4fa42832e6d6b2c623787914713158850
praneet <praneet18.ark@gmail.com>
2025-05-13 17:27:33 +0530

    code changes

--- server/count/wsServer.js
+++ server/count/wsServer.js
@@ -2,7 +2,6 @@ import { WebSocketServer } from "ws";
 
 const port = Number(process.env.WEBSOCKET_SERVER_PORT || 8081)
 const wss = new WebSocketServer({ port });
-console.log(Number(port))
 wss.on("connection", (ws) => {
   console.log("connected");
   broadcastCount();
