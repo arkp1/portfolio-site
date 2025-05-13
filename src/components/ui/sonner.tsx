236d121ad0aa001910775c82d6728dada9e8b833
praneet <praneet18.ark@gmail.com>
2025-05-14 00:10:46 +0530

    updated toast

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -12,8 +12,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
         } as React.CSSProperties
       }
